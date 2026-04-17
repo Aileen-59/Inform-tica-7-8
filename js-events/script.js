b610d79af418aa977d9e95f68ec320cf70af199f
Aileen <aileenmagadanruiz@gmail.com>
2026-04-17 17:29:32 +0000

    articles of faith

---
+++ js-events/script.js
@@ -0,0 +1,19 @@
+// function greet(name){
+//     //console.log(`Hello ${name}`);
+//     console.log("Hello" + name);
+// }
+
+// greet("Aileen");
+
+// function addNumbers(num1, num2){
+//     let sum = num1 + num2;
+//     alert("Sum is equal to " + sum);
+// }
+
+// //addNumbers(6,7)
+
+function articlesOfFatih(Article of Faith 1,Article of Faith 2,Article of Faith 3){
+    console.log(We believe in God, the Eternal Father, and in His Son, Jesus Christ, and in the Holy Ghost)
+    console.log(We believe that men will be punished for their own sins, and not for Adam’s transgression.)
+    console.log(We believe that through the Atonement of Christ, all mankind may be saved, by obedience to the laws and ordinances of the Gospel.)
+}
