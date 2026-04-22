7ec65edd9202ae9f8a04f07e6d8b9020666cf5aa
Aileen <aileenmagadanruiz@gmail.com>
2026-04-22 20:19:15 +0000

    variables index.html

---
+++ js-variables/script.js
@@ -0,0 +1,24 @@
+function function1(){
+    const name = "Aileen Magadan";
+const color = "Blue";
+
+let location = "Mexico";
+let mood = "Sleepy";
+
+console.log("Name:" + name);
+console.log("Favorite color:" + color);
+console.log("Current Location:" + location);
+console.log("Mood:" + mood);
+
+}
+
+function function2(){
+    let companyName = "
+
+console.log()
+}
+
+
+function function3(){
+alert( "The variable value should be 5!");
+}
