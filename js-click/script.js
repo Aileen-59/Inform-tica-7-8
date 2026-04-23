66d83ee650592d9f69f951435b552593e58a6184
Aileen <aileenmagadanruiz@gmail.com>
2026-04-23 20:19:13 +0000

    click game

--- js-click/script.js
+++ js-click/script.js
@@ -1,5 +1,7 @@
-function showTime() {
-  let timeNow = new Date();
-	let heading = document.querySelector('h1');
-	header.innerHTML = timeNow;
+let i = 0;
+
+function count(){
+i++;//i = i + 1
+let header = document.querySelector("h1");
+header.innerHTML = i;
 }
