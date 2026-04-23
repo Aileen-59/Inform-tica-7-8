fcea5f00375f2888d105b8d38a4540fb1f8631e6
Aileen <aileenmagadanruiz@gmail.com>
2026-04-23 17:08:46 +0000

    js variables

--- js-variables/script.js
+++ js-variables/script.js
@@ -1,24 +1,38 @@
 function function1(){
-    const name = "Aileen Magadan";
-const color = "Blue";
+    const name = "Aileen ";
+    const color = "Blue";
 
-let location = "Mexico";
-let mood = "Sleepy";
+    let location = "Colonia Juarez";
+    let mood = "Sleepy";
 
-console.log("Name:" + name);
-console.log("Favorite color:" + color);
-console.log("Current Location:" + location);
-console.log("Mood:" + mood);
+    console.log("Name:" + name);
+    console.log("Favorite color:" + color);
+    console.log("Current Location:" + location);
+    console.log("Mood:" + mood);
+
+    mood="Happy"
+    console.log("New mood: "+ mood);
+
 
 }
 
 function function2(){
-    let companyName = "
+    let companyName = "Coca-Cola";
+    let foundingYear = "1886";
+    let isActive = "true";
+    let fundingAmount;
 
-console.log()
+    console.log("Company Name:" + companyName);
+    console.log("Founding Year:" + foundingYear);
+    console.log("Is Active:" + isActive);
+    console.log("Funding Amount:" + fundingAmount);
+
 }
 
 
 function function3(){
+    let variable = 5
+    
+
 alert( "The variable value should be 5!");
 }
