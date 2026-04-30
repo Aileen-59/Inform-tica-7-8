98d97103e63e7e29ff732125500e16e6cfdadc00
Aileen <aileenmagadanruiz@gmail.com>
2026-04-30 18:06:56 +0000

    index...

---
+++ rps/script.js
@@ -0,0 +1,15 @@
+let subtitle = document.querySelector();
+  let number = Math.random();
+  let result;
+
+  if(number <= 0.5){
+    result= rock;
+  }else if(number > 0.5){
+    result= paper;
+  }else{
+    result= scissors;
+  }
+
+
+
+
