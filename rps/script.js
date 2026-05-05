d8a608c6c56cb6508ec3580ae62edd64d45adb1f
Aileen <aileenmagadanruiz@gmail.com>
2026-05-05 16:29:11 +0000

    index.html

--- rps/script.js
+++ rps/script.js
@@ -1,15 +1,31 @@
-let subtitle = document.querySelector();
-  let number = Math.random();
-  let result;
-
-  if(number <= 0.5){
-    result= rock;
-  }else if(number > 0.5){
-    result= paper;
-  }else{
-    result= scissors;
+function game(userChoice){
+  let randomNum = Math.random();
+  let compChoice;
+  if(randomNum < 0.33){
+    compChoice = "Rock";
+  }else if(randomNum < 0.66){
+    compChoice = "Paper";
+  }else if (randomNum < 1){
+    compChoice = "Scissiors";
   }
 
+  console.log(compChoice);
+
+  let result = document.querySelector("#result")
+  if(userChoice == compChoice){
+    result.innerHTML = "Tie";
+  }else if(userChoice == "Rock"){
+    if(compChoice == "Paper"){
+      result.innerHTML = "You Lose";
+    }else{
+      result.innerHTML = "You won";
+    }
+  }
+}
 
 
+  // document.addEventListener('DOMContentLoaded', function(){
+  //   document.querySelector("#rock-button").onClick = ("Rock");
+
+  // });
 
