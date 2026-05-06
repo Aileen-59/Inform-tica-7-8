fc9ed612fd64a92414057ffdb7d56f796e201130
Aileen <aileenmagadanruiz@gmail.com>
2026-05-06 19:35:18 +0000

    yaaaaaaaaa

--- greeting/script.js
+++ greeting/script.js
@@ -1,6 +1,10 @@
-document.addEventListener("DOMContentLoaded", function()
-    document.querySelector("form").addEventListener(") = function(){
-    let name = document.querySelector("#name").value;
-    alert(name);
+document.addEventListener("DOMContentLoaded", function(){
+    document.querySelector("form").onsubmit = function(){
+        let name = document.querySelector("#name").value;
+        console.log("Hi" + name);
 
-});
+        document.querySelector("h1").innerHTML = "Hi" + name;
+
+        return false;
+    }
+})
